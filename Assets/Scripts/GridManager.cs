620bbcf08a8a408d2311ef4c16b2548e408ef7dc
Bezilon <bezilon@gmail.com>
2015-11-28 09:10:01 +0200

    Migrating already existing code and models to the project

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridManager : MonoBehaviour {
+
+	public int SizeX;
+	public int SizeY;
+	public float HexRadius;
+	[HideInInspector]
+	public HexInfo[,] HexArray;
+	public GameObject DefaultTile;
+
+	public Vector2 PlayerGridPosition;
+	public Vector3 PlayerCubePosition {
+		get {
+			return new Vector3(PlayerGridPosition.x, PlayerGridPosition.y, -PlayerGridPosition.x-PlayerGridPosition.y);
+		}
+	}
+
+	// Use this for initialization
+	void Start() {
+		PlayerGridPosition = new Vector2(0.0F,0.0F);
+		CreateBasicGrid();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		//Highlight Player position
+		HighlightPlayerPosition();
+	}
+
+	private void CreateBasicGrid() {
+		float YStep = Mathf.Sqrt( HexRadius*HexRadius - HexRadius*HexRadius/4 );
+		float XStep = HexRadius - Mathf.Sqrt( HexRadius*HexRadius - YStep*YStep )/2;
+		float OddRowPush = 0.0F;
+		float GlobalX = 0.0F;
+		float GlobalY = 0.0F;
+		GameObject CurrentTile;
+
+		HexArray = new HexInfo[SizeX, SizeY];
+
+		for(int GridX = 0; GridX < SizeX; GridX++) {
+			OddRowPush = ( GridX % 2 == 1 ) ? ( YStep / 2 ) : 0;
+			for(int GridY = 0; GridY < SizeY; GridY++) {
+				try {
+					GlobalX = (GridY*YStep)+OddRowPush;
+					GlobalY = -GridX*XStep;
+					CurrentTile = (GameObject)Instantiate(DefaultTile, new Vector3(GlobalX, 0, GlobalY), Quaternion.identity);
+					CurrentTile.name = "[" + GridY + "," + GridX + "]";
+					CurrentTile.transform.SetParent(this.transform);
+					HexArray[GridY,GridX] = new HexInfo(CurrentTile, GlobalX, GlobalY, GridX, GridY, true, 1);
+				}
+				catch( UnityException e ) {
+					Debug.Log( "Caught an error: " + e );
+				}
+			}
+		}
+	}
+
+	private void GenerateTerrain() {
+
+	}
+
+	private void LoadTerrain() {
+		
+	}
+
+	public Vector3 GetGlobalsOfTile(int x, int y) {
+		return HexArray[x,y].GlobalCoordinates;
+	}
+
+	public void HighlightPlayerPosition() {
+		HexArray[(int)PlayerGridPosition.x,(int)PlayerGridPosition.y].ColorHexTile(new Color(1, 0, 0, 1));
+	}
+	
+}
---
+++ Assets/Scripts/HexInfo.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexInfo {
+	//The GameObject of this HexTile
+	private GameObject Hex;
+	// Global Position
+	public Vector3 GlobalCoordinates;
+	// Position in the Grid
+	public Vector2 GridCoordinates;
+	// Cube coordinates
+	public Vector3 CubeCoordinates {
+		get {
+			return new Vector3(GridCoordinates.x, GridCoordinates.y, -GridCoordinates.x-GridCoordinates.y);
+		}
+	}
+	// Hex Properties
+	public Vector2[] GridNeighbours;
+	public Vector3[] CubeNeighbours;
+	public bool Passable;
+	public bool BlockingTheView;
+	public int Cost;
+
+	// Default Constructor
+	public HexInfo() {
+		Passable = true;
+		Cost = 1;
+	}
+	// Consturctor that knows the global and the grid position
+	public HexInfo(GameObject Hex_, float x, float y, int GrX, int GrY) {
+		Hex = Hex_;
+		GlobalCoordinates = new Vector3(x, 0, y);
+		GridCoordinates = new Vector2(GrX, GrY);
+		GridNeighbours = new Vector2[] {
+			new Vector2(GrX+1, GrY), new Vector2(GrX+1, GrY-1), new Vector2(GrX, GrY-1),
+			new Vector2(GrX-1, GrY), new Vector2(GrX-1, GrY+1), new Vector2(GrX, GrY+1)
+		};
+		CubeNeighbours = new Vector3[] {
+			new Vector3(CubeCoordinates.x+1, CubeCoordinates.y-1,  CubeCoordinates.z), new Vector3(CubeCoordinates.x+1,  CubeCoordinates.y, CubeCoordinates.z-1), new Vector3(CubeCoordinates.x, CubeCoordinates.y+1, CubeCoordinates.z-1),
+			new Vector3(CubeCoordinates.x-1, CubeCoordinates.y+1,  CubeCoordinates.z), new Vector3(CubeCoordinates.x-1,  CubeCoordinates.y, CubeCoordinates.z+1), new Vector3(CubeCoordinates.x, CubeCoordinates.y-1, CubeCoordinates.z+1)
+		};
+		Passable = true;
+		BlockingTheView = false;
+		Cost = 1;
+	}
+	// Consturctor that knows the global and the grid position
+	public HexInfo(GameObject Hex_, float x, float y, int GrX, int GrY, bool Passable_, int Cost_) {
+		Hex = Hex_;
+		GlobalCoordinates = new Vector3(x, 0, y);
+		GridCoordinates = new Vector2(GrX, GrY);
+		GridNeighbours = new Vector2[] {
+			new Vector2(GrX+1, GrY), new Vector2(GrX+1, GrY-1), new Vector2(GrX, GrY-1),
+			new Vector2(GrX-1, GrY), new Vector2(GrX-1, GrY+1), new Vector2(GrX, GrY+1)
+		};
+		CubeNeighbours = new Vector3[] {
+			new Vector3(CubeCoordinates.x+1, CubeCoordinates.y-1,  CubeCoordinates.z), new Vector3(CubeCoordinates.x+1,  CubeCoordinates.y, CubeCoordinates.z-1), new Vector3(CubeCoordinates.x, CubeCoordinates.y+1, CubeCoordinates.z-1),
+			new Vector3(CubeCoordinates.x-1, CubeCoordinates.y+1,  CubeCoordinates.z), new Vector3(CubeCoordinates.x-1,  CubeCoordinates.y, CubeCoordinates.z+1), new Vector3(CubeCoordinates.x, CubeCoordinates.y-1, CubeCoordinates.z+1)
+		};
+		Passable = Passable_;
+		BlockingTheView = false;
+		Cost = Cost_;
+	}
+
+	public void ColorHexTile(Color NewColor) {
+		Hex.GetComponent<Renderer>().material.color = NewColor;
+	}
+}
